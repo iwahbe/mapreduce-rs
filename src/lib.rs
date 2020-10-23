6972e696d15fbe3551579def8a8ac354691100c6
Ian Wahbe <ian@wahbe.com>
2020-10-23 18:59:53 +0200

    Clean up debug code in MR_main

--- src/lib.rs
+++ src/lib.rs
@@ -212,29 +212,12 @@ unsafe impl<T> std::marker::Send for ThreadSafe<T> {}
 /// }
 /// ```
 /// This prints the number of words in each file.
-///
-/// To instead print the number of words found in all files, we can write
-/// ```C
-/// void Reduce(char *key, Getter get_next, int partition_number) {
-///     int count = 0;
-///     char *value;
-///     while ((value = get_next(key, partition_number)) != NULL)
-///         count += atoi(value);
-///     char *res = (char*)malloc(sizeof(char)*10);
-///     if (!strcmp(key, "TOTAL_COUNT")) {
-///         printf("Total count %d", count)
-///     } else {
-///         snprintf(res, 10, "%d", count);
-///         MR_Emit("TOTAL_COUNT", value);
-///     }
-/// }
-/// ```
 pub extern "C" fn MR_Run(
     argc: c_int,
     argv: *const *const c_char,
     map: Mapper,
     num_mappers: c_int,
-    reduce: Reducer,
+    _reduce: Reducer,
     num_reducers: c_int,
     partition: Partitioner,
 ) {
@@ -246,6 +229,7 @@ pub extern "C" fn MR_Run(
         );
         return;
     }
+    // Must be first call to EMITTED
     EMITTED.setup(partition, num_reducers.try_into().unwrap());
     let mappers = ThreadPool::new(num_mappers as usize).unwrap();
     let file_names = CArray::from(argv);
@@ -256,25 +240,7 @@ pub extern "C" fn MR_Run(
     mappers.wait(0); // wait until there are no threads
 
     let _reducers = ThreadPool::new(num_reducers as usize).unwrap();
-    for (i, name) in file_names.iter_to(argc as usize).enumerate().skip(1) {
-        unsafe {
-            println!("argv[{}]: {:?}", i, CStr::from_ptr(*name));
-        }
-    }
-    println!(
-        "num_mappers, num_reducers: {:?}, {:?}",
-        num_mappers as u64, num_reducers as i64
-    );
-    let file_name = CString::new("my map file_name").unwrap();
-    let map_ptr = file_name.as_bytes_with_nul().as_ptr();
-    map(map_ptr as *const c_char);
-    let reduce_key = CString::new("reduce key").unwrap();
-    let reduce_key_ptr = reduce_key.as_bytes_with_nul().as_ptr();
-    reduce(
-        reduce_key_ptr as *const c_char,
-        getter as *const extern "C" fn(*const c_char, c_int) -> *const c_char,
-        7,
-    );
+    // Reduce code goes here
 
     println!("MR_RUN called");
 }
